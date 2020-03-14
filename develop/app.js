9d4bbd6c3c6cdf2c91866c3b3ef03a00d478d207
Annaruth McBride <armcbride17@gmail.com>
2020-03-14 11:39:29 -0500

    added google maps key and console log

--- develop/app.js
+++ develop/app.js
@@ -2,3 +2,8 @@ var autoKey = "2526bdb078c64052edacba2a56ac385d";
 var queryURLBase =
   "https://sandbox-api.brewerydb.com/v2/locations/?key=" + autoKey;
 console.log(queryURLBase);
+
+var mapKey = "AIzaSyAn329Spd1q1DQlk4-nhlHx2q_dEr7jids";
+var queryURLBaseTwo =
+  "https://maps.googleapis.com/maps/api/js?key=" + mapKey + "&callback=initMap";
+console.log(queryURLBaseTwo);
