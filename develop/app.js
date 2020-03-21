5dfe103e0c695185efac7231d7a85d1bdb0ce452
Annaruth McBride <armcbride17@gmail.com>
2020-03-21 09:51:55 -0500

    merging

--- develop/app.js
+++ develop/app.js
@@ -1,9 +1,19 @@
+//global variables & API keys
 var autoKey = "2526bdb078c64052edacba2a56ac385d";
 var queryURLBase =
   "https://sandbox-api.brewerydb.com/v2/locations/?key=" + autoKey;
 console.log(queryURLBase);
 
-//global variables
+var queryTerm = " ";
+var weatherKey = "3fef80c9e928a329e2b89a8041b3fe71";
+var weatherURL =
+  "https://api.openweathermap.org/data/2.5/weather?q=" +
+  queryTerm +
+  "&appid=" +
+  weatherKey;
+
+console.log(weatherURL);
+
 //create search button with user input
 $("#searchBtn").on("click", function(e) {
   e.preventDefault();
@@ -17,16 +27,9 @@ $("#clearAll").on("click", function(e) {
   console.log("you click me");
 });
 //take input and stringify
+
 //create section to write information pulled from keys
 
 //functions
 
-var queryTerm = " ";
-var weatherKey = "3fef80c9e928a329e2b89a8041b3fe71";
-var weatherURL =
-  "https://api.openweathermap.org/data/2.5/weather?q=" +
-  queryTerm +
-  "&appid=" +
-  weatherKey;
 
-console.log(weatherURL);
