1e2f92a3fab0fda2a5fe7cad06530993886e99cf
charlenne alexander <charlennep@gmail.com>
2020-03-22 15:55:43 -0500

    created dynamicly the cards for the weather and working on the brewery on a separete sheet. also created the dropdown btn to seletec the ammount of searchs.

---
+++ develop/brewery.js
@@ -0,0 +1,9 @@
+var autoKey = "b1b92e789ec6ed213bedfaec1a833a6c";
+// var queryURL = "https://beermapping.com/webservice/loccity/";
+
+function brewery() {
+  var queryURL = "https://beermapping.com/webservice/loccity/";
+
+  console.log(queryURL);
+}
+console.log(brewery());
